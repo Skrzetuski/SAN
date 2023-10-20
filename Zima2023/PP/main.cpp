abc8828f5e2f8d4fab49f078042afa1afbde2090
Krystian Guminski <krystian.guminsk@gmail.com>
2023-10-20 13:47:45 +0200

    Po zajeciach

--- Zima2023/PP/main.cpp
+++ Zima2023/PP/main.cpp
@@ -6,8 +6,29 @@ Kompilowanie i uruchomienie: clang++ -std=c++20 main.cpp && ./a.out
 
 #include <iostream>
 #include <vector>
+#include <random>
 
 using namespace std;
+
+// https://en.cppreference.com/w/cpp/numeric/random
+int generate_number_in_range(int start, int end){
+
+  default_random_engine engine(random_device{}());
+  uniform_int_distribution<int> dist(start, end);
+  return dist(engine);
+}
+
+char generate_special_char(){
+  return generate_number_in_range(33, 47);
+}
+
+char generate_number(){
+  return generate_number_in_range(48, 57);
+}
+
+char generate_letter(){
+  return generate_number_in_range(97, 122);
+}
 
 int main(){
 
@@ -18,17 +39,24 @@ int main(){
                             "4. Znaki specjalne[&*][{}]",
 			    "0. Koniec"};
 			    
-  unsigned short int choice = 100;
+
+
+  cout<<generate_special_char()<<endl;
+  cout<<generate_number()<<endl;
+  cout<<generate_letter()<<endl;
+  cout<<char(toupper(generate_letter()))<<endl;
+
+  // unsigned short int choice = 100;
   
-  while(choice){
-    for (auto &output : options){
-        cout<<output<<endl;
-      }
+  // while(choice){
+  //   for (auto &output : options){
+  //       cout<<output<<endl;
+  //     }
       
-    cin>>choice;
-    cout<<"Wybrano opcje: "<<choice<<"\n\n";
+  //   cin>>choice;
+  //   cout<<"Wybrano opcje: "<<choice<<"\n\n";
       
-  }
+  // }
 
 			                      
 }
